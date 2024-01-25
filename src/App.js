04c1b55e7ac47d304edb843480d9d691ad9e75c3
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-01-25 07:28:57 -0300

    ADD: state object

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import TableHead from "./components/TableHead.js";
 import TableFoot from "./components/TableFoot.js";
 import TableBody from "./components/TableBody.js";
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    heroes: [
+      {
+        id: "1",
+        name: "Abel",
+        book: "Genesis",
+        testament: "Old"
+      },
+      {
+        id: "2",
+        name: "Enoch",
+        book: "Genesis",
+        testament: "Old"
+      },
+      {
+        id: "3",
+        name: "Noah",
+        book: "Genesis",
+        testament: "Old"
+      }
+    ]
+  };
+  render() {
+    return (
     <div className="App">
       <table className="tabela">
         <TableHead />
         <TableFoot />
-        <TableBody />
+        <TableBody heroes={ this.state.heroes } />
       </table>
     </div>
   );
+}
 };
 
 export default App;
