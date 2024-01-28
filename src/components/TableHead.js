19a33381effdfb1a978afa924d475fbd7d498892
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-01-28 15:01:58 -0300

    ADD: sort alphabetical order

--- src/components/TableHead.js
+++ src/components/TableHead.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const TableHead = () => (
+const TableHead = (props) => (
     <thead>
         <tr>
             <th colSpan="4">Heros of Faith Table</th>
         </tr>
         <tr>
-            <th>Name</th>
+            <th>Name
+                <div className="container-arrows">
+                    <div onClick={ () => props.sortCrescent() }>&#8593;</div>
+                    <div onClick={ () => props.sortDecrescent() }>&#8595;</div>
+                </div>
+            </th>
             <th>Book</th>
             <th>Testament</th>
         </tr>
